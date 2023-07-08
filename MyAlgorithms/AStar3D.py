b662272fb42a6ce2402aec1b5ed2ac7593fd6068
neco kriel <necokriel@gmail.com>
2023-07-08 16:42:19 +1000

    update files

--- MyAlgorithms/AStar3D.py
+++ MyAlgorithms/AStar3D.py
@@ -5,6 +5,7 @@ import time
 import numpy as np
 import multiprocessing as mproc
 
+from moviepy.editor import VideoClip
 from mayavi import mlab
 from scipy.spatial import KDTree
 
@@ -200,8 +201,8 @@ def main():
   dict_adj = genAdjDict_parallel(verts, faces)
   ## define start and end points
   tree = KDTree(verts)
-  _, start_vi = tree.query([20, 20,  0])
-  _, end_vi   = tree.query([ 0, 10, 20])
+  _, start_vi = tree.query([10, 20, 0])
+  _, end_vi   = tree.query([20, 0, 20])
   start_point = verts[start_vi,:]
   end_point   = verts[end_vi,:]
   printPoint(start_point, "start point: ")
@@ -220,26 +221,42 @@ def main():
     y_geo = [ pos[1] for pos in soln_path ]
     z_geo = [ pos[2] for pos in soln_path ]
   else: print("Could not find a solution!")
+  end_time = time.time()
+  ## initialise figure
+  fig = mlab.figure(size=(500,500), bgcolor=(1,1,1))
   ## plot isosurface
-  mlab.triangular_mesh(verts[:,0], verts[:,1], verts[:,2], faces, representation="surface")
-  mlab.triangular_mesh(verts[:,0], verts[:,1], verts[:,2], faces, representation="mesh")
+  # mlab.triangular_mesh(verts[:,0], verts[:,1], verts[:,2], faces, representation="surface")
+  mlab.triangular_mesh(verts[:,0], verts[:,1], verts[:,2], faces, representation="mesh", tube_radius=0.05)
   ## plot geodesic
   if len(soln_path_grouped) > 0:
-    mlab.points3d(start_point[0], start_point[1], start_point[2])
-    mlab.points3d(end_point[0], end_point[1], end_point[2])
-    mlab.plot3d(x_geo, y_geo, z_geo, range(len(x_geo)), tube_radius=0.25)
-  ## plot pseudo-axis
-  mlab.points3d(0, 0, 0)
-  mlab.points3d(2, 0, 0)
-  mlab.points3d(0, 4, 0)
-  mlab.points3d(0, 0, 6)
-  mlab.plot3d([0, 2.0], [0, 0], [0, 0], tube_radius=0.1)
-  mlab.plot3d([0, 0], [0, 4.0], [0, 0], tube_radius=0.1)
-  mlab.plot3d([0, 0], [0, 0], [0, 6.0], tube_radius=0.1)
-  mlab.points3d(res,res,res)
-  ## show canvas
-  mlab.show()
-  end_time = time.time()
+    mlab.points3d(start_point[0], start_point[1], start_point[2], color=(0,0,0))
+    mlab.points3d(end_point[0], end_point[1], end_point[2], color=(0,0,0))
+    # mlab.plot3d(x_geo, y_geo, z_geo, range(len(x_geo)), tube_radius=0.25)
+    mlab.plot3d(x_geo, y_geo, z_geo, color=(0,0,0), tube_radius=0.2)
+  # ## plot pseudo-axis
+  # mlab.points3d(0, 0, 0)
+  # mlab.points3d(2, 0, 0)
+  # mlab.points3d(0, 4, 0)
+  # mlab.points3d(0, 0, 6)
+  # mlab.plot3d([0, 2.0], [0, 0], [0, 0], tube_radius=0.1)
+  # mlab.plot3d([0, 0], [0, 4.0], [0, 0], tube_radius=0.1)
+  # mlab.plot3d([0, 0], [0, 0], [0, 6.0], tube_radius=0.1)
+  # mlab.points3d(res,res,res)
+  ## animate
+  duration = 7
+  def make_frame(t):
+    mlab.view(
+      azimuth    = 360 * t/duration,
+      elevation  = 70,
+      distance   = 65,
+      focalpoint = [res//2, res//2, res//2],
+      figure     = fig
+    )
+    return mlab.screenshot(antialiased=True)
+  animation = VideoClip(make_frame, duration=duration)
+  animation.write_gif("geodesic.gif", fps=30)
+  # ## show canvas
+  # mlab.show()
   print(f"Elapsed time: {end_time - start_time:.3f} seconds")
 
 
--- MyAlgorithms/SolveTSP.py
+++ MyAlgorithms/SolveTSP.py
@@ -1,7 +1,14 @@
+## ###############################################################
+## MODULES
+## ###############################################################
 import copy
 import numpy as np
-from mayavi import mlab
+import matplotlib.pyplot as plt
 
+
+## ###############################################################
+## HELPER FUNCTIONS
+## ###############################################################
 def euclidean(p1, p2):
   return np.sqrt(
     (p2[0] - p1[0])**2 +
@@ -19,27 +26,18 @@ def plotPath(list_points):
   z = [ point[2] for point in list_points ]
   mlab.plot3d(x, y, z, range(len(list_points)), tube_radius=0.25)
 
-def solveTSP_nn(list_points):
-  ''' Solve Travelling Salesman Problem using Nearest Neighbor method
-  '''
-  list_points_unsorted = copy.deepcopy(list_points)
-  current_point = list_points_unsorted.pop(0)
-  list_points_sorted = [ current_point ]
-  while len(list_points_unsorted) > 0:
-    next_point_index = np.argmin([
-      euclidean(current_point, next_point)
-      for next_point in list_points_unsorted
-      if not(next_point == current_point)
-    ])
-    next_point = list_points_unsorted[next_point_index]
-    list_points_unsorted.remove(next_point)
-    list_points_sorted.append(next_point)
-    current_point = next_point
-  return list_points_sorted
+def plotPDF(ax, list_data, color="blue"):
+  list_dens, list_bin_edges = np.histogram(list_data, bins=20, density=True)
+  list_dens_norm = np.append(0, list_dens / list_dens.sum())
+  ax.fill_between(list_bin_edges, list_dens_norm, step="pre", alpha=0.2, color=color)
+  ax.plot(list_bin_edges, list_dens_norm, drawstyle="steps", color=color)
 
-## program main
-def main():
-  list_points = [
+
+## ###############################################################
+## POINTS ALONG SURFACE PERIMETER
+## ###############################################################
+def getListPoints():
+  return [
     [0.497, 0.000, 15.000],
     [0.977, 0.000, 28.000],
     [0.000, 0.000, 29.000],
@@ -186,17 +184,82 @@ def main():
     [28.503, 29.000, 14.000],
     [29.000, 28.494, 15.000]
   ]
+
+
+## ###############################################################
+## NEAREST NEIGHBOR SOLUTION
+## ###############################################################
+def solveTSP_nn(list_points):
+  ''' Solve Travelling Salesman Problem using Nearest Neighbor method
+  '''
+  list_points_unsorted = copy.deepcopy(list_points)
+  current_point = list_points_unsorted.pop(0)
+  list_points_sorted = [ current_point ]
+  list_dist_travelled = []
+  while len(list_points_unsorted) > 0:
+    next_point_index = np.argmin([
+      euclidean(current_point, next_point)
+      for next_point in list_points_unsorted
+      if not(next_point == current_point)
+    ])
+    next_point = list_points_unsorted[next_point_index]
+    list_dist_travelled.append(euclidean(current_point, next_point))
+    list_points_unsorted.remove(next_point)
+    list_points_sorted.append(next_point)
+    current_point = next_point
+  return list_points_sorted, list_dist_travelled
+
+
+## ###############################################################
+## MOVING AVERAGE, NEAREST NEIGHBOR SOLUTION
+## ###############################################################
+def solveTSP_nn_stable(list_points):
+  list_points_unsorted = copy.deepcopy(list_points)
+  current_point = list_points_unsorted.pop(0)
+  prev_point = [0, 0, 0]
+  list_points_sorted = [ current_point ]
+  list_dist_travelled = []
+  while len(list_points_unsorted) > 0:
+    next_point_index = np.argmin([
+      euclidean(current_point, next_point) + euclidean(prev_point, next_point)
+      for next_point in list_points_unsorted
+      if not(next_point == current_point)
+    ])
+    next_point = list_points_unsorted[next_point_index]
+    list_dist_travelled.append(euclidean(current_point, next_point))
+    list_points_unsorted.remove(next_point)
+    list_points_sorted.append(next_point)
+    prev_point = current_point
+    current_point = next_point
+  return list_points_sorted, list_dist_travelled
+
+
+## ###############################################################
+## PROGRAM MAIN
+## ###############################################################
+def main():
+  list_points = getListPoints()
   ## find shortest path connecting points
   print("Solving travelling salesman problem...")
-  list_points_sorted_nn = solveTSP_nn(list_points)
-  ## plot data
-  print("Plotting data...")
-  plotPoints(list_points)
-  plotPath(list_points_sorted_nn)
-  mlab.show()
+  list_points_sorted, list_dist_travelled = solveTSP_nn(list_points[::-1])
+  # ## plot solution path
+  # from mayavi import mlab
+  # print("Plotting solution path...")
+  # plotPoints(list_points)
+  # plotPath(list_points_sorted_nn)
+  # mlab.show()
+  ## plot pdf of distances travelled
+  print("Plotting PDF of distances travelled...")
+  fig, ax = plt.subplots()
+  plotPDF(ax, list_dist_travelled)
+  fig.savefig("dists.png")
 
-## program entry point
+
+## ###############################################################
+## PROGRAM ENTRY POINT
+## ###############################################################
 if __name__ == "__main__":
   main()
 
-## end of program
+
+## END OF PROGRAM
